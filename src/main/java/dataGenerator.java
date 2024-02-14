7ad7af9ce1537f5b22fda354adcdf74da26d55e8
kr0man0va <67516645+kr0man0va@users.noreply.github.com>
2024-02-14 16:05:12 -0500

    ___

--- src/main/java/dataGenerator.java
+++ src/main/java/dataGenerator.java
@@ -2,12 +2,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import java.io.OutputStreamWriter;
 
 public class dataGenerator {
 
     public static void main(String[] args) throws Exception{
         //Create data file with points
 
+        //Change to 3000
         int dataSize = 3000;
         int maxValueXY = 5000;
 
@@ -15,9 +21,16 @@ public class dataGenerator {
     }
 
     public static void writeDatasetToCSV(int size, int maxValue, String filename) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(filename))) {
+        Configuration conf = new Configuration();
+
+        try (FileSystem fs = FileSystem.get(conf)) {
+            Path hdfsPath = new Path(filename);
+            FSDataOutputStream outputStream = fs.create(hdfsPath);
+
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream));
+
             // Write CSV header
-            writer.println("id,x,y");
+//            writer.println("id,x,y");
 
             Random random = new Random();
 
@@ -25,8 +38,12 @@ public class dataGenerator {
             for (int i = 0; i < size; i++) {
                 int x = random.nextInt(maxValue + 1);
                 int y = random.nextInt(maxValue + 1);
-                writer.println(i+ "," + x + "," + y);
+                writer.println(x + "," + y);
             }
+
+            writer.close();
+            outputStream.close();
+
         } catch (IOException e) {
             System.out.println("CSV file failed.");
         }
